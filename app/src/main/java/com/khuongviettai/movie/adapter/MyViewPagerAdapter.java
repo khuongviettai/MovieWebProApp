f1b1f7eae3de1a449939fb54c0e04934ec24f150
Khương Viết Tài <khuongtai@MacBook-Pro.local>
2023-05-14 05:21:43 +0700

    update

---
+++ app/src/main/java/com/khuongviettai/movie/adapter/MyViewPagerAdapter.java
@@ -0,0 +1,41 @@
+package com.khuongviettai.movie.adapter;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.khuongviettai.movie.fragment.AccountFragment;
+import com.khuongviettai.movie.fragment.FavoriteFragment;
+import com.khuongviettai.movie.fragment.HistoryFragment;
+import com.khuongviettai.movie.fragment.HomeFragment;
+
+public class MyViewPagerAdapter extends FragmentStateAdapter {
+
+    public MyViewPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 1:
+                return new FavoriteFragment();
+
+            case 2:
+                return new HistoryFragment();
+
+            case 3:
+                return new AccountFragment();
+
+            default:
+                return new HomeFragment();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 4;
+    }
+}
